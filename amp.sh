cca7c7e65ad034c4a20bdd214ccb4fc35ba85b3d
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-04 13:18:35 +0800

    Update and rename Ind.sh to amp.sh

---
+++ amp.sh
@@ -0,0 +1,406 @@
+clear
+echo $cyan "SEDANG MASUK KE TOOLS NYA "
+sleep 5
+clear
+echo $cyan " SELMANT DATANG DI SCRIP TOOLS BY FAIZUL "
+termux-open https://www.youtube.com/channel/UCSxkYGsMV5DvXz4GQJskl9Q
+sleep 10
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo $or "
+╭━┳━╭━╭━╮%s╮╲╲╲╲╲╲%s { 🔘ＦＡＩＺＵＬ ＢＯＦＴＥＮ ]
+┃┈┈┈┣▅╋▅┫┃%s╲╲╲╲╲╲%s { 🔘２３ ＳＣＲＩＰ ＴＯＯＬＳ Ｖ９.９ ]
+┃┈┃┈╰━╰━━━━━━╮%s╲╲%s { 🔘ＮＯＴＥ🔴 ＲＥＣＯＤＥ ]
+╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣%s╲%s { 🔴ＩＮＤＯＮＥＳＩＡ ]
+╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉%s╲%s { 🔴ＳＵＬＴＥＮＧ ]
+╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤%s╲%s { 🔴ＡＭＰＡＮＡ ２０２１ ]
+╲┃┈┈┈┈╭━┳━━━━╯%s╲╲%s╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗ 🔓 :  082271426251 ]
+╲┣━━━━━━┫%s╲╲╲╲╲╲╲%s║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
+╲┃┈┈┈┈┈┈┃%s╲╲╲╲╲╲╲%s╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩" 
+echo $ij "   
+          ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+          🔓 : ２３ ＴＯＯＬＳ ＨＡＣＫＩＧ Ｖ９.９ : 🔓
+          ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+      ▬▬▬▬▬▬▬▬▬▬▬
+┠──[1]🔓 HACK TARGET V1 
+      ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[2]🔓 HACK FB PREMIUM
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[3]🔓 PERUSAK HP
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[4]🔓 HACK TARGET V2 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[5]🔓 SPAM BRUTAL 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[6]🔓 MEMBUKA FILE TERKUNCI BS4 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[7]🔓 HACK AKUN G7 PROJECT 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[8]🔓 JADWAL SHOLAT 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[9]🔓 HACK TARGET V3 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[10]🔓 CLON INDIA 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[11]🔓 INSTALL BAHAN 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[12]🔓 MEMBUAT DEFICE 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[13]🔓 CLON AKUN TANPA LOGIN 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[14]🔓 INSTALL METASPLOIT 
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[15]🔓 MENGHAPUS DATA SCRIP TERMUX
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[16]🔓 NEMBAK APK GOFOOD
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[17]🔓 BOT AUTO RECTION FACEBOOK
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[18]🔓 MENDEGARKAN MUSIK DI TERMUX
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[19]🔓 HACK WIFI 1.9
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[20]🔓 NONTON FILEM BOKEP
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[21]🔓 SPAM WATSAPPS UPDATE 9.9
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[22]🔓 HACK GRUB FB 9.0
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+        ▬▬▬▬▬▬▬▬▬▬▬
+┠──[23]🔓 INSTALL PHISING 9.7
+        ▬▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+┗────[99] EXIT/PROGRAM🔴"
+echo '\033[35;1m'
+read -p "localhost@Pilih Nomor > " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sleep 2
+clear
+echo $cyan "MENGGINSTALK COK"
+echo $cyan "PASTIKAN JARINGGAN BAGUS"
+sleep 10
+pkg install git
+git clone https://github.com/MRA27/FBNEWV2
+cd FBNEWV2
+python2 MRA27.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+sleep 2
+clear
+echo $cyan "SCRIP SUDAH COID"
+sleep 30
+echo $cyan "BELUM RILIS SAYANG TUNGGU AJA YAA "
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+sleep 2
+clear
+echo $cyan "MENGINSTALL COK"
+sleep 10
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+sleep 2
+echo $cyan " MENGINSTALL COK "
+sleep 10
+pkg install git
+git clone https://github.com/V4N654T/fb-hacker
+cd fb-hacker
+python2 fb.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+sleep 2
+echo $cyan " MENGGINSTALL COK "
+sleep 10
+echo $cyan " BELUM RILIS LAGI SAYANG YA ALLAH "
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+sleep 2
+echo $cyan " MENGGINSTALL COK "
+sleep 10
+pkg install git
+git clone https://github.com/Bl4ckDr460n/MarshBase
+cd MarshBase
+python2 MarshBase.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then 
+clear
+sleep 2
+echo $cyan " MENGGINSTALL COK "
+sleep 10
+pkg install git
+git clone https://github.com/G7-Hckr/G7-Project
+cd G7-Project
+sh al.sh
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+sleep 2
+echo $cyan " MENGGINSTALL COK "
+sleep 10
+pkg install git
+git clone https://github.com/karjok/sholat/
+cd sholat
+pip install -r modul.txt
+python sholat.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+sleep 5
+echo $cyan " MENGGINSTALL COK "
+sleep 10
+pkg install git
+git clone https://github.com/FR13ND8/BruteFb
+cd BruteFb
+python2 brute.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+sleep 5
+echo $cyan " MENGINSTALL ABA "
+sleep 10
+pkg install git
+pkg install php
+pkg install curll
+git clone https://github.com/hearthackerBabar/FbCloningIndia
+cd FbCloningIndia
+python2 FbIndia.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+sleep 5
+echo $cyan " HARAP MENUGGU SEDANG MENGINSTALL "
+sleep 10
+pkg install lolcat
+pkg install nano
+pkg install git
+git clone
+pip2 install meyzu
+pip2 install requests
+pip2 install mechanize
+pip2 install php 
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+echo $cyan "MENGGINSTTALL COK"
+sleep 10
+pkg instalk git
+git clone https://github.com/G7-Hckr/G7Deface
+cd G7Deface
+cd G7Deface
+sh dfc.sh
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+echo $cyan " SEDANG MENGGINSTALL HARAP TUNGGU"
+sleep 50
+pip2 install requests
+pip2 install mechanize
+pkg install git
+echo $cyan "SCRIP MASI EROR
+git clone https://github.com/NoobHackersCOM/Sensei
+cd Sensei
+python2 main.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+echo $cyan " SABAR SOBAT MENGGINSTALL "
+sleep 40
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+echo $cyan " MOHOM BERSABAR LAGI MEMUAT DATANYA"
+sleep 50
+pip2 install mc
+pkg install mc
+mc
+fi
+
+if [ $bro = 16 ] || [ = $bro = 16 ]
+then
+clear
+echo $cyan " MENGINSTALL HARAP TUNGGU
+sleep 20
+pkg install git
+echo $cyan " BELUM RILIS LAGI"
+fi
+
+if [ $bro = 17 ] || [ = $bro =17 ]
+then
+clear
+echo $cyab " MENGGINSTALL SCRIP"
+sleep 20
+pip2 install mechanize
+echo $cyan " SCRIP UPADTE SELESAI SELMAT MENGUNAKAN"
+git clone git clone https://github.com/AMVengeance/FB-React.git
+chmod +x FB-React -R
+cd FB-React
+./start
+fi
+
+if [ $bro = 18 ] || [ = $bro = 18 ]
+then
+clear
+echo $cyan " MENGINSTALL CUK"
+sleep 20
+pkg install mpv
+mpv /lokasi/musik/musik.mp3
+fi
+
+if [ $bro = 19 ] || [ = $bro = 19 ]
+then
+clear
+echo $cyan " MENGGINSTALL CUK"
+sleep 20
+echo $cyan " Hack WIFI DI TERMUX MANA BISA HALU KALIAN"
+pkg install git
+git clone https://github.com/derv82/wifite
+cd wifite
+chmod 777 wifite.py
+python2 wifite.py
+fi
+
+if [ $bro = 20 ] || [ = $bro = 20 ]
+then
+clear
+echo $cyan " MENGGINSTALL TOOLS BOKEP"
+pkg install git
+pip2 install mecahnize
+git clone https://github.com/Faizulboften/ToolsBokep
+cd ToolsBokep
+sh viral.sh
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+echo $cyan " MENGINSTALL DULU🔓"
+sleep 10
+pkg install git
+pip2 install lolcat
+git clone https://github.com/sandiwijayani1/SpamWa
+cd SpamWa
+python3 spam.py
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+echo $cyan " MENGINSTALL DULU 🔓"
+sleep 10
+echo $cyan " YALLAH AMPUNILAH DOSA MEREKA🔓"
+pip2 install requests
+git clone https://github.com/Mr-NemrMedo/fb-group-hack
+cd fb-group-hack
+python2 fb-group-hack.py
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+echo $cyan "MENGINSTALL PHISING🔓"
+sleep 10
+pkg install git
+pkg install nano
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "SUBSCIRIBEE TY HTC CTR GAMING"
+termux-open https://www.facebook.com/profile.php?id=100027772032939
+sleep 30
+exit
+fi
